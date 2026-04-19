53c0e0bea19432c26b67451d1d948763449b57a3
Adrian Josue Contreras Yauri <adrianjosuecontrerasyauri@gmail.com>
2026-04-18 22:43:52 -0500

    fist commit

--- Universidad/Universidad/urls.py
+++ Universidad/Universidad/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Academico.views import *
+from academico.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
