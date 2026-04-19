343dbcde01e2540bf56751891cfae21f770f5662
Adrian Josue Contreras Yauri <adrianjosuecontrerasyauri@gmail.com>
2026-04-18 23:55:28 -0500

    fist commit

--- Universidad/Academico/apps.py
+++ Universidad/Academico/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class AcademicoConfig(AppConfig):
-    name = 'Academico'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Universidad.Academico'
