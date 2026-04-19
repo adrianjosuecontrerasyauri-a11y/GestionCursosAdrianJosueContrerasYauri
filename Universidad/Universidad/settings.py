623fd32299b250684abade366f79446eccfa9d0d
Adrian Josue Contreras Yauri <adrianjosuecontrerasyauri@gmail.com>
2026-04-19 00:01:08 -0500

    fist commit

--- Universidad/Universidad/settings.py
+++ Universidad/Universidad/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Universidad.urls'
+ROOT_URLCONF = 'Universidad.Universidad.urls'
 
 TEMPLATES = [
     {
